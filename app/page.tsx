7b5213da6caccb8bea619a912960964297488ad4
Ilham Muhammad <ilhamuh97@gmail.com>
2025-07-22 12:12:24 +0200

    add hero and fade-in motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import fetchPrayerTime from '@/actions/prayerTime';
 import PrayerTimesCard from '@/components/prayerTimesCard/prayerTimesCard';
+import Hero from '@/components/hero/hero';
 
 export default async function Home() {
   const prayerTimeData = await fetchPrayerTime();
 
   return (
     <div>
+      <Hero />
       <PrayerTimesCard prayerTimes={prayerTimeData} />
     </div>
   );
